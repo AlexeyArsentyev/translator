c35e8e38313bdf3f6ebdbb815b18679113bdf2d9
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2025-04-08 19:32:45 +0300

    change app name

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TranslatorForm from './components/Form/Form.jsx';
 
 export default () => (
   <div className="main-wrapper">
-    <h1>Funny translator</h1>
+    <h1>Broken translator</h1>
     <p className="description">
       Get your text through a Google translator a bunch of times to get some strange result
       <br></br>
