ac9c694f32897a27eb10c92f608dbf03a249d440
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2025-04-08 19:31:19 +0300

    add description

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import TranslatorForm from './components/Form/Form.jsx';
 export default () => (
   <div className="main-wrapper">
     <h1>Funny translator</h1>
+    <p className="description">
+      Get your text through a Google translator a bunch of times to get some strange result
+      <br></br>
+      Why would you need this? No idea. You decide.
+    </p>
     <main>
       <TranslatorForm />
     </main>
